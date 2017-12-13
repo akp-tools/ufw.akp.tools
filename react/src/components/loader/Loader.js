f7933900e155aaf16687f637f49bf9fbd448ffcf
Austin Peterson <austin@akpwebdesign.com>
2017-12-13 00:50:20 -0500

    fix(loader): use https for placeholder images

--- react/src/components/loader/Loader.js
+++ react/src/components/loader/Loader.js
@@ -5,8 +5,8 @@ import './Loader.css';
 const Loader = ({ visible }) => (
   <div className={`Loader ${visible ? 'visible' : ''}`}>
     <div className="Logo">
-      <img src="http://via.placeholder.com/250x250" alt="akp.tools logo" /><br />
-      <img src="http://via.placeholder.com/100x100" alt="loading indicator" />
+      <img src="https://via.placeholder.com/250x250" alt="akp.tools logo" /><br />
+      <img src="https://via.placeholder.com/100x100" alt="loading indicator" />
     </div>
   </div>
 );
