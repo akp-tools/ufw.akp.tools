433844d2c3678363c420139ed54fa76d62b34aec
Austin Peterson <austin@akpwebdesign.com>
2017-12-24 16:02:59 -0500

    feat(functions): add aggregate data transaction on every new ufw block

--- functions/index.js
+++ functions/index.js
@@ -81,6 +81,51 @@ exports.onNewUfwBlock = functions.firestore.document('/ufwBlocks/{id}').onCreate
         range,
       },
     };
-    return event.data.ref.set(blk, { merge: true });
+    return event.data.ref.set(blk, { merge: true }).then(() => firestore.collection('geopoints').add({ geo, country, subcountry }))
+      .then(() => {
+        const cityRef = firestore.collection('cities').doc(city);
+        const subcountryRef = firestore.collection('regions').doc(subcountry);
+        const countryRef = firestore.collection('countries').doc(country);
+        const geoRef = firestore.collection('geopoints').doc(`${geo.latitude}/${geo.longitude}`);
+        return firestore.runTransaction((t) => {
+          let cityCount = 0;
+          let subcountryCount = 0;
+          let countryCount = 0;
+          let geoCount = 0;
+
+          return t.get(cityRef)
+            .then((doc) => {
+              cityCount = doc.data() || cityCount;
+              cityCount += 1;
+              return t.get(subcountryRef);
+            })
+            .then((doc) => {
+              subcountryCount = doc.data() || subcountryCount;
+              subcountryCount += 1;
+              return t.get(countryRef);
+            })
+            .then((doc) => {
+              countryCount = doc.data() || countryCount;
+              countryCount += 1;
+              return t.get(geoRef);
+            })
+            .then((doc) => {
+              geoCount = doc.data() || geoCount;
+              geoCount += 1;
+            })
+            .then(() => {
+              t.update(cityRef, cityCount)
+                .update(subcountryRef, subcountryCount)
+                .update(countryRef, countryCount)
+                .update(geoRef, geoCount);
+            });
+        })
+          .then((res) => {
+            console.log('Transaction success', res);
+          })
+          .catch((err) => {
+            console.log('Transaction failure:', err);
+          });
+      });
   });
 });
