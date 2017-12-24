075a7843125a852b82aaa14ab2c7fa2224f955ea
Austin Peterson <austin@akpwebdesign.com>
2017-12-24 14:42:34 -0500

    feat(map): make lines fade out toward target based on how many lines there are overall

--- react/src/components/map/DeckGLOverlay.js
+++ react/src/components/map/DeckGLOverlay.js
@@ -15,8 +15,8 @@ export default class DeckGLOverlay extends Component {
     const layer = new ArcLayer({
       id: 'grid',
       data,
-      getSourceColor,
-      getTargetColor,
+      getSourceColor: d => getSourceColor(d, false, data.length),
+      getTargetColor: d => getSourceColor(d, true, data.length),
       getSourcePosition: d => (d.geo ? [d.geo.longitude, d.geo.latitude] : [0, 0]),
       getTargetPosition: d => [-81.5427402, 35.922479],
     });
@@ -30,16 +30,18 @@ DeckGLOverlay.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-const getSourceColor = (d, t) => {
-  const str = `${d.country}`;
+const getSourceColor = (d, t, dataLength) => {
+  let str = `${d.country}`;
+  if (str === 'United States') {
+    str = `${d.subcountry}, ${str}`;
+  }
 
   const hash = crypto.createHash('md5').update(str).digest('hex').slice(0, 6);
   const red = parseInt(hash.slice(0, 2), 16);
   const green = parseInt(hash.slice(2, 4), 16);
   const blue = parseInt(hash.slice(4, 6), 16);
-  const alpha = t ? 0 : 255;
+  const alpha = t ? dataLength / 1000 * 255 : 255;
+
 
   return [red, green, blue, alpha];
 };
-
-const getTargetColor = d => getSourceColor(d, true);
