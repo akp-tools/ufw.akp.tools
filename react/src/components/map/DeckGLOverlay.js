74f8152f19f77c19b7512c4cccae1832596e0faf
Austin Peterson <austin@akpwebdesign.com>
2017-12-23 22:01:30 -0500

    feat(map): convert ScreenGridLayer to ArcLayer, use hash of country name to color

--- react/src/components/map/DeckGLOverlay.js
+++ react/src/components/map/DeckGLOverlay.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import DeckGL, { ScreenGridLayer } from 'deck.gl';
+import DeckGL, { ArcLayer } from 'deck.gl';
+
+const crypto = require('crypto');
 
 export default class DeckGLOverlay extends Component {
   render() {
-    const { viewport, cellSize, data } = this.props;
+    const { viewport, data } = this.props;
 
     if (!data) {
       return null;
     }
 
-    const layer = new ScreenGridLayer({
+    const layer = new ArcLayer({
       id: 'grid',
       data,
-      minColor: [0, 255, 0, 0],
-      maxColor: [0, 255, 0, 240],
-      getPosition: d => (d.geo ? [d.geo.longitude, d.geo.latitude] : [0, 0]),
-      cellSizePixels: cellSize,
+      getSourceColor,
+      getTargetColor,
+      getSourcePosition: d => (d.geo ? [d.geo.longitude, d.geo.latitude] : [0, 0]),
+      getTargetPosition: d => [-81.5427402, 35.922479],
     });
 
     return <DeckGL {...viewport} layers={[layer]} />;
@@ -25,6 +27,18 @@ export default class DeckGLOverlay extends Component {
 
 DeckGLOverlay.propTypes = {
   viewport: PropTypes.objectOf(PropTypes.number).isRequired,
-  cellSize: PropTypes.number.isRequired,
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
+
+const getSourceColor = (d) => {
+  const str = `${d.country}`;
+
+  const hash = crypto.createHash('md5').update(str).digest('hex').slice(0, 6);
+  const red = parseInt(hash.slice(0, 2), 16);
+  const green = parseInt(hash.slice(2, 4), 16);
+  const blue = parseInt(hash.slice(4, 6), 16);
+
+  return [red, green, blue, 255];
+};
+
+const getTargetColor = d => getSourceColor(d);
